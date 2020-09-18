a71b51297a7433811274048b095bd31fc37c9004
ryagif <21254@bridgend.ac.uk>
2020-09-18 12:57:07 +0000

    yo

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+  print ("Greetings! How are you doing?")
+  print ("Nice and dandy like cotton candy!")
+  print ("Oh, how interesting. Tell me more!")
+  print ("Just went shopping")
+  print ("Thanks for sharing!")
+
 
 if __name__ == '__main__':
     main()
